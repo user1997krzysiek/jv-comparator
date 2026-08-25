a89c6085c2d713536fb0f18d2a885a298c740225
Krzysiek <krzysztof.orawski@wp.pl>
2026-08-25 20:31:57 +0200

    service

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,18 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Comparator<Employee> comparator = Comparator
+                .comparing(Employee::getAge)
+                .thenComparing(Employee::getName);
+
+        Set<Employee> sortedEmployees = new TreeSet<>(comparator);
+        sortedEmployees.addAll(employees);
+        return sortedEmployees;
     }
 }
